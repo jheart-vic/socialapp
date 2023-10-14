6293446236d16980fbcb21de69bd98fb7f638783
jheart-vic <adebowalevictorjheart@gmail.com>
2023-10-14 17:24:53 +0100

    correct mistake

--- server/index.js
+++ server/index.js
@@ -59,10 +59,10 @@ app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
-app.use(express.static(path.join(__dirname, "..", "client", "build")));
+app.use(express.static(path.join(__dirname, "..", "frontend", "build")));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "frontend", "build", "index.html"));
 });
 
 
